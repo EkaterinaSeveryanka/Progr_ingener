dc78ce125fe66a3d8f5ebfdd50a283697b21315f
EkaterinaSeveryanka <117996969+EkaterinaSeveryanka@users.noreply.github.com>
2023-01-26 22:15:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ t = pipeline("Image Transformer", model="facebook/deit-base-patch16-224")
 def root():
     return {"message": "Hello World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
 @app.post("/Image Transformer/")
 def predict(item: Item):
     return t(item.text)[0]
